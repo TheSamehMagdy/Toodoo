1557177d3e923416f739b48e830549747789a593
Sameh Magdy <thesamehmagdy@gmail.com>
2018-08-20 02:43:54 +0200

    Add click X to delete todo function

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 $("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
+//Click X to delete todo
+$("ul").on("click", "li span", function(event){
+	event.stopPropagation();
+	$(this).parent().fadeOut(400, function(){
+		$(this).remove();
+	})
+});
